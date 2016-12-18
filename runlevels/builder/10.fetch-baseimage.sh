0b93f85e7443778fb9f77f66026f09353f87779b
W-Mark Kubacki <wmark@hurrikane.de>
2016-12-18 01:34:37 +0100

    utilize builder.mountPoints to mount the cache directory

--- runlevels/builder/10.fetch-baseimage.sh
+++ runlevels/builder/10.fetch-baseimage.sh
@@ -3,9 +3,14 @@
 isLevelEnabled "debug" && set -x
 
 set -euo pipefail
-: ${CACHEDIR:="/dgr/aci-home/cache"}
+: ${CACHEDIR:="/cache"}
 : ${BASEIMAGE:="ubuntu-16.04-linux-$(dpkg --print-architecture).aci"}
 : ${ACI_HOST:="https://s.blitznote.com/aci"}
+
+if [[ ! -s "${CACHEDIR}/${BASEIMAGE}" ]]; then
+  find / -name 'cache' -type d
+  exit 9
+fi
 
 curl --fail --progress-bar --location --remote-time --create-dirs \
   -H "Accept: application/aci, application/octet-stream" \
